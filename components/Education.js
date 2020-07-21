9a792bc768143f2412eb56aab36034a0ba03f4f8
jake tribe <jacobtribe@me.com>
2020-07-21 10:55:19 -0600

    Adds Favicon

--- components/Education.js
+++ components/Education.js
@@ -11,19 +11,9 @@ export default function Education() {
     <Container>
       <h2>Education</h2>
       <h4>Brigham Young University</h4>
-      <ul>
-        <li>GPA 3.51</li>
-        <li>Member of BYU Tech Club and Association for Information Systems</li>
-      </ul>
+      <p>BS in Information Systems</p>
       <h4>Woods Cross High School</h4>
-      <ul>
-        <li>Graduated High Honor Roll</li>
-        <li>ACT 32/36, 97th Percentile</li>
-        <li>
-          Participated as a team member for the Basketball, Golf, and Tennis
-          teams
-        </li>
-      </ul>
+      <p>Graduated High Honor Roll </p>
     </Container>
   )
 }
--- components/Experience.js
+++ components/Experience.js
@@ -6,44 +6,24 @@ const Container = styled('div')`
   background: #3498db;
 `
 
+const Left = styled('div')``
+
+const Right = styled('div')`
+  display: inline;
+`
+
 export default function Experience() {
   return (
     <Container>
       <h2>Experience</h2>
-      <h4>Nozzle.io</h4>
-      <ul>
-        <li>
-          Responsible for website management and development using React.js
-        </li>
-        <li>
-          Participated in using GitHub to manage version control of Company
-          website
-        </li>
-        <li>
-          Collaborated with industry leaders to learn new technologies more
-          efficiently
-        </li>
-      </ul>
+      <Left>
+        <h4>Nozzle.io</h4>
+        <p>Front End Intern</p>
+      </Left>
+      <Right>Feb 2020 - Current</Right>
       <h4>The Naccarato Leadership Center</h4>
-      <ul>
-        <li>
-          Responsible for a 500% increase in opening rate for a weekly email
-          marketing campaign
-        </li>
-        <li>
-          Responsible for comprehensive redesign and development of company
-          website
-        </li>
-
-        <li>
-          Lead for database management. Ensuring data is clean, current and
-          organized driving efficiency and better analytics
-        </li>
-        <li>
-          Lead for all technical troubleshooting and solutions for the
-          organization
-        </li>
-      </ul>
+      <p>I.T. Specialist</p>
+      <Right>May 2019 - Dec 2019</Right>
     </Container>
   )
 }
