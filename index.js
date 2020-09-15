f3adf2c28ffe07305fb6d0cc508e7efc15be1fcf
jake tribe <jacobtribe@me.com>
2020-09-15 12:50:00 -0600

    fixes

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import Home from './pages'
-
-// Render your app
-if (typeof document !== 'undefined') {
-  ReactDOM.render(<Home />, document.getElementById('root'))
-}
