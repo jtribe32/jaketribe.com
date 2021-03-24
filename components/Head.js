e846c0de89d9fa964c3b3fae549181272104a6cd
jake tribe <jtribe32@gmail.com>
2021-03-24 14:50:02 -0600

    removes html flash

--- components/Head.js
+++ components/Head.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default () => {
   return (
@@ -38,6 +39,24 @@ export default () => {
         href="/favicon-16x16.png"
       />
       <link rel="manifest" href="/site.webmanifest" />
+
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     </Head>
   )
 }
